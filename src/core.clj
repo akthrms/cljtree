2b15c875a5c81b6a3cfceaa8833f5a9a69d303b1
akthrms <akt.hrms@gmail.com>
2021-08-12 14:13:57 +0900

    update

--- src/core.clj
+++ src/core.clj
@@ -7,17 +7,21 @@
               ["-l" "--limit DEPTH" "Limit depth" :parse-fn #(Integer/parseInt %)]
               ["-h" "--help" "Show this help message"]])
 
+(defn help-message
+  [summary]
+  (str "`tree` powered by Clojure" \newline
+       \newline
+       "USAGE" \newline
+       "  cljtree [dirname] : Show children of dirname. default dirname is pwd." \newline
+       \newline
+       "OPTIONS" \newline
+       summary))
+
 (defn -main [& args]
   (let [{:keys [options arguments summary errors]} (cli/parse-opts args options)]
     (cond
       errors (println errors)
-      (:help options) (do (println "`tree` powered by Clojure")
-                          (println "")
-                          (println "USAGE")
-                          (println "  cljtree [dirname] : Show children of dirname. default dirname is pwd.")
-                          (println "")
-                          (println "OPTIONS")
-                          (println summary))
+      (:help options) (println (help-message summary))
       :else (let [root (if (empty? arguments) "." (first arguments))]
               (println root)
               (tree root "" (->Options (or (:limit options) 10)
